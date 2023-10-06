cef32adaefea81399d74f7bf4f58168a11360f94
Cüneyt Kaya <kayacuneyt2542@gmail.com>
2023-10-06 13:39:38 +0300

    Create CategoryValidator

---
+++ ShoppingListProject.BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using ShoppingListProject.EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingListProject.BusinessLayer.ValidationRules
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Kategori adı boş geçilemez.");
+            RuleFor(x => x.Name).MaximumLength(50).WithMessage("Kategori adı en fazla 50 karakter olabilir.");
+        }
+    }
+
+}
